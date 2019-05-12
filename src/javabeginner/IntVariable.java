1a16b868d919a3f0a1c663ca8a703f96c945fc44
ashraf <mdashrafkhanqa@gmail.com>
2019-05-12 09:57:16 -0400

    New class added

---
+++ src/javabeginner/IntVariable.java
@@ -0,0 +1,14 @@
+package javabeginner;
+
+public class IntVariable {
+
+    public static void main(String[] args) {
+
+
+        int Banana=100;
+        int Apple=110;
+
+        System.out.println(Banana);
+        System.out.println(Apple);
+    }
+}
